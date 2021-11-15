1643890dce257ff1dc40aedc30042236b17ba763
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-11-15 16:50:26 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+data "terraform_remote_state" "image_name" {
+  backend = "remote"
+
+  config = {
+    organization = "jhp0204"
+    workspaces = {
+      name = "PACKER-terraform-gcp"
+    }
+  }
+}
+
+resource "aws_instance" "jenkins-demo" {
+  count         = "1"
+  ami           = data.terraform_remote_state.image_name.outputs.image_name
+  instance_type = "t2.micro"
+
+  tags {
+    Name  = "Packer-test"
+  }
+
+  key_name  = "${var.key_name}"
+}
